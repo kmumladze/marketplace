c5340245a685d76820994fd8b695941e6c943171
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-02-04 13:14:36 +0400

    style: products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,21 @@
 export default function Products({ products }) {
   return (
-    <ul>
+    <div className="grid grid-cols-4 gap-4">
       {products.map((product) => (
-        <li key={product.id}>
-          <img src={product.thumbnail} alt="" />
-        </li>
+        <div className="flex flex-col items-start mx-4" key={product.id}>
+          <img
+            className="rounded-xl bg-stone-200"
+            src={product.thumbnail}
+            alt=""
+          />
+          <h1 className="font-bold">{product.title}</h1>
+          <p>{product.brand}</p>
+          <p className="font-bold">{product.price} $</p>
+          <button className="bg-blue-500 p-3 rounded-2xl py-1 hover:text-stone-50 cursor-pointer">
+            See Details
+          </button>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
