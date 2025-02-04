2430ab434e219c3f109a47bed1b735b8f498e5c0
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-02-04 12:34:08 +0400

    refactor: header moved into new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import marketplaceLogo from "./assets/marketplace.svg";
-import messageLogo from "./assets/message.svg";
-import heartLogo from "./assets/heart.svg";
-import cartLogo from "./assets/cart.svg";
-import login from "./assets/login.svg";
+import { useEffect } from "react";
+
+import Header from "./components/Header.jsx";
 
 export default function App() {
-  return (
-    <header className="flex gap-4 justify-around my-4">
-      <div className="flex items-center gap-3 border-4 border-blue-500 hover:border-blue-700 p-5 rounded-xl">
-        <img
-          className="max-w-16"
-          src={marketplaceLogo}
-          alt="marketplace logo"
-        />
-        <h3 className="font-mono text-xl font-bold text-stone">Marketplace</h3>
-      </div>
-      <div className="flex w-9 gap-3">
-        <img src={messageLogo} alt="" />
-        <img src={heartLogo} alt="" />
-        <img src={cartLogo} alt="" />
-      </div>
-    </header>
-  );
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch("https://dummyjson.com/products");
+      const resData = await response.json();
+      console.log(resData);
+    }
+    fetchProducts();
+  }, []);
+  return <Header />;
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import marketplaceLogo from "../assets/marketplace.svg";
+import messageLogo from "../assets/message.svg";
+import heartLogo from "../assets/heart.svg";
+import cartLogo from "../assets/cart.svg";
+
+export default function Header() {
+  return (
+    <header className="flex gap-4 justify-around my-4">
+      <div className="flex items-center gap-3 border-4 border-blue-500 hover:border-blue-700 p-5 rounded-xl">
+        <img
+          className="max-w-16"
+          src={marketplaceLogo}
+          alt="marketplace logo"
+        />
+        <h3 className="font-mono text-xl font-bold text-stone">Marketplace</h3>
+      </div>
+      <div className="flex w-9 gap-3">
+        <img src={messageLogo} alt="" />
+        <img src={heartLogo} alt="" />
+        <img src={cartLogo} alt="" />
+      </div>
+    </header>
+  );
+}
