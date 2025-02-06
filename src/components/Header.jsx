5f7610dd002566ef3d9d553092a8de43f38d8d9c
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-02-06 13:37:17 +0400

    style: header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,35 +6,31 @@ import logIn from "../assets/login.svg";
 
 export default function Header({ setSearch, search }) {
   return (
-    <header className="flex gap-4 justify-around my-4 items-center">
+    <header className="flex flex-col md:flex-row gap-4 justify-between md:justify-around items-center my-4">
       <div className="flex items-center gap-3 border-4 border-blue-500 hover:border-blue-700 p-5 rounded-xl cursor-pointer">
-        <img
-          className="max-w-16"
-          src={marketplaceLogo}
-          alt="marketplace logo"
-        />
+        <img className="max-w-8" src={marketplaceLogo} alt="marketplace logo" />
         <h3 className="font-mono text-xl font-bold text-stone">Marketplace</h3>
       </div>
       <input
         type="text"
         placeholder="Search"
-        className="bg-stone-200 h-20 rounded-xl p-4"
+        className="bg-stone-200 h-20 rounded-xl p-4 mt-4 md:mt-0"
         onChange={(element) => setSearch(element.target.value)}
         value={search}
       />
-      <div className="flex w-9 gap-3">
+      <div className="flex justify-center items-center gap-3 w-9 mt-4 md:mt-0">
         <img className="cursor-pointer" src={messageLogo} alt="" />
         <img
           className="cursor-pointer
           hover:bg-red-500
           rounded-xl"
           src={heartLogo}
-          alt=""
+          alt="heart logo"
         />
         <img className="cursor-pointer" src={cartLogo} alt="" />
       </div>
-      <div className="flex gap-2 border-4 rounded-xl p-4 border-blue-400 cursor-pointer hover:bg-blue-300">
-        <img className="w-7" src={logIn} alt="log in logo" />
+      <div className="flex gap-2 border-4 rounded-xl p-4 border-blue-400 cursor-pointer hover:bg-blue-300 mt-4 md:mt-0">
+        <img className="w-5" src={logIn} alt="log in logo" />
         <h3>Log In</h3>
       </div>
     </header>
