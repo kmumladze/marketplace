4295eaf5bcba51c41770269b09366379880c9761
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-02-12 14:31:36 +0400

    feat: blog comments

---
+++ src/components/BlogComments.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+
+import { AiFillLike } from "react-icons/ai";
+
+export default function Blogcomments() {
+  const { id } = useParams();
+  const [blogComments, setBlogComments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchBlogComments() {
+      try {
+        const response = await fetch(
+          `https://dummyjson.com/comments/post/${id}`
+        );
+        const resData = await response.json();
+        // console.log(resData);
+        setBlogComments(resData.comments);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setTimeout(() => {
+          setLoading(false);
+        }, 200);
+      }
+    }
+    fetchBlogComments();
+  }, [id]);
+
+  return (
+    <div className="mt-6 bg-stone-200 dark:bg-gray-500 p-4 rounded-lg w-1/2">
+      {loading ? (
+        <p>Loading comments...</p>
+      ) : (
+        <>
+          <h3 className="font-bold text-lg mb-2 flex gap-2 items-center">
+            Comments:
+            <span className="bg-red-500 text-white dark:text-black w-2 h-2 px-5 py-4 flex items-center justify-center rounded-2xl text-sm">
+              {blogComments.length}
+            </span>
+          </h3>
+          {blogComments.length > 0 ? (
+            <ul className="space-y-3">
+              {blogComments.map((comment) => (
+                <li
+                  key={comment.id}
+                  className="p-3 bg-gray-100 dark:bg-gray-600 rounded-lg"
+                >
+                  <p className="font-semibold">{comment.user.username}</p>
+                  <p>{comment.body}</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No comments yet.</p>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import UserInfo from "./UserInfo.jsx";
 import Header from "./Header.jsx";
 import { Link } from "react-router";
+import Blogcomments from "./BlogComments.jsx";
 
 export default function BlogDetails() {
   const { id } = useParams();
@@ -28,14 +29,23 @@ export default function BlogDetails() {
   return (
     <>
       <Header />
-      <main className="flex justify-center items-center min-h-screen bg-blue-200 dark:bg-gray-900">
+      <main className="flex flex-col justify-center items-center min-h-screen bg-blue-200 dark:bg-gray-900">
         <div className="flex flex-col p-6 gap-4 bg-stone-200 rounded-xl w-1/2 justify-center items-center dark:bg-gray-500">
           <h1 className="font-bold text-2xl">{blog.title}</h1>
           <Link to={`/users/${blog.userId}`}>
             <UserInfo userId={blog.userId} />
           </Link>
           <p>{blog.body}</p>
+          <div className="flex gap-6 justify-end">
+            <p className="text-green-500">
+              Likes: {blog.reactions?.likes || 0}
+            </p>
+            <p className="text-red-500">
+              Dislikes: {blog.reactions?.dislikes || 0}
+            </p>
+          </div>
         </div>
+        <Blogcomments />
       </main>
     </>
   );
