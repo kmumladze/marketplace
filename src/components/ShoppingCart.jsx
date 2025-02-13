adbe5cb14866560fad12bb4db9d10f3b9637b16d
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-02-13 15:46:32 +0400

    feat: the number of selected carts

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,9 +3,7 @@ import { useContext } from "react";
 import { CartContext } from "../providers/CartProvider";
 
 export default function ShoppingCart() {
-  const { cart } = useContext(CartContext);
-
-  console.log(cart);
+  const { cart, updateItemQuantity } = useContext(CartContext);
 
   const totalPrice = cart.reduce(
     (acc, product) => acc + product.price * product.quantity,
@@ -22,15 +20,33 @@ export default function ShoppingCart() {
           {cart.map((cart, index) => (
             <li
               key={index}
-              className="flex gap-2 p-3 bg-gray-100 dark:bg-gray-600 rounded-lg justify-between"
+              className="flex items-center gap-2 p-3 bg-gray-100 dark:bg-gray-600 rounded-lg justify-between"
             >
               <p className="font-bold">{cart.title}</p>
               <p className="font-bold font-mono">${cart.price}</p>
+
+              <div className="flex items-center gap-1 bg-stone-200 rounded-xl p-2">
+                <button
+                  className="bg-transparent border-none rounded-md cursor-pointer"
+                  onClick={() => updateItemQuantity(cart.id, -1)}
+                >
+                  -
+                </button>
+                <span>{cart.quantity}</span>
+                <button
+                  className="bg-transparent border-none rounded-md cursor-pointer"
+                  onClick={() => updateItemQuantity(cart.id, 1)}
+                >
+                  +
+                </button>
+              </div>
             </li>
           ))}
         </ul>
       )}
-      <p>{formattedPrice}</p>
+      <p className="text-right mt-2">
+        Cart Total: <strong>{formattedPrice}</strong>
+      </p>
     </div>
   );
 }
