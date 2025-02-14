5babb312a6d67fff50056a573f800a6263b2f4b0
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-02-14 15:29:15 +0400

    feat: blog read more scroll

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -4,27 +4,37 @@ import Header from "../components/Header.jsx";
 import { Link } from "react-router";
 
 export default function BlogPage() {
+  const [curPage, setCurPage] = useState(1);
   const [blogs, setBlogs] = useState([]);
+
+  const [isFollowed, setIsFollowed] = useState(false);
+
+  const loadMoreOnClick = () => {
+    setCurPage((prev) => prev + 1);
+  };
 
   useEffect(() => {
     async function fetchBlogs() {
       try {
-        const response = await fetch("https://dummyjson.com/posts?limit=0");
+        const response = await fetch(
+          `https://dummyjson.com/posts?limit=10&skip=${(curPage - 1) * 10}`
+        );
         const resData = await response.json();
         console.log(resData);
-        setBlogs(resData.posts);
+        setBlogs([...blogs, ...resData.posts]);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     }
     fetchBlogs();
-  }, []);
+  }, [curPage]);
 
   console.log(blogs);
 
   return (
     <>
       <Header />
+
       <div className="flex flex-col gap-5 dark:bg-gray-900 dark:text-white">
         {blogs.map((blog, index) => (
           <div
@@ -38,7 +48,7 @@ export default function BlogPage() {
               {/* <p>Author: {blog.userId || "No user info"}</p> */}
               <div>
                 <Link to={`/blog/${blog.id}`}>
-                  <button className="bg-blue-500 text-stone-950 p-4 rounded-xl">
+                  <button className="bg-blue-500 text-stone-950 p-4 rounded-xl hover:text-white hover:transition-colors">
                     Read Blog
                   </button>
                 </Link>
@@ -55,6 +65,15 @@ export default function BlogPage() {
             </div>
           </div>
         ))}
+
+        <div className="flex justify-center">
+          <button
+            className="bg-blue-500 text-stone-950 px-6 py-4 m-4 rounded-xl hover:text-white hover:transition-colors"
+            onClick={loadMoreOnClick}
+          >
+            Load More
+          </button>
+        </div>
       </div>
     </>
   );
--- src/pages/UserDetailsPage.jsx
+++ src/pages/UserDetailsPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../components/Header";
+import { Avatar } from "@heroui/react";
 
 export default function UserDetailsPage() {
   const { userId } = useParams();
@@ -27,11 +28,12 @@ export default function UserDetailsPage() {
       <Header />
       <main className="min-h-screen dark:bg-gray-500 dark:text-white">
         <div className="flex flex-col justify-evenly items-center md:flex-row p-6 dark:bg-gray-500">
-          <img
-            className="bg-stone-100 rounded-full w-1/4 dark:bg-opacity-20"
+          <Avatar
+            isBordered
             src={user.image}
-            alt="user image"
+            className="w-1/4 h-1/4 dart:bg-opacity-20"
           />
+
           <div className="flex flex-col gap-4">
             <h1 className="font-bold text-2xl mt-10">
               {user.firstName} {user.lastName}
