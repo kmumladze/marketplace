2a2a18a7cd810d7cd0f7f15dc76caf1c70e1a931
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-02-26 17:58:03 +0400

    feat: date countdown

--- src/components/DealsOfTheMonth.jsx
+++ src/components/DealsOfTheMonth.jsx
@@ -1,14 +1,46 @@
+import { useState, useEffect } from "react";
+
 import dealsofthemonth from "../assets/dealsofthemonth.jpg";
 import { Link } from "react-router";
 
-const countDown = [
-  { label: "Days", value: 120 },
-  { label: "Hours", value: 18 },
-  { label: "Mins", value: 15 },
-  { label: "Secs", value: 10 },
-];
-
 export default function DealsOfTheMonth() {
+  const [time, setTime] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTime(countdown());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [time]);
+
+  const countdown = () => {
+    const currentDate = new Date();
+    const futureDate = new Date("07/17/2025");
+    const diff = futureDate - currentDate;
+
+    if (diff > 0) {
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 66 * 60)) % 24);
+      const minutes = Math.floor((diff / (1000 * 60)) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+
+      return [
+        { label: "Days", value: days },
+        { label: "Hours", value: hours },
+        { label: "Mins", value: minutes },
+        { label: "Secs", value: seconds },
+      ];
+    }
+
+    return [
+      { label: "Days", value: 0 },
+      { label: "Hours", value: 0 },
+      { label: "Mins", value: 0 },
+      { label: "Secs", value: 0 },
+    ];
+  };
+
   return (
     <div className="flex flex-col md:flex-row items-center w-full justify-between p-6 bg-gray-100 rounded-xl shadow-lg">
       <div className="flex flex-col gap-6 md:w-1/2">
@@ -19,8 +51,9 @@ export default function DealsOfTheMonth() {
           Don't miss out on this month's hottest deals! Enjoy discounts on your
           favorite products—limited time only. Shop now and save big!
         </p>
+        {/* <h1>{timeRemaining}</h1> */}
         <div className="flex gap-4 justify-center md:justify-start">
-          {countDown.map((item, index) => (
+          {time.map((item, index) => (
             <div
               key={index}
               className="border-2 p-4 rounded-xl text-center bg-white shadow-md w-20"
@@ -34,7 +67,7 @@ export default function DealsOfTheMonth() {
         </div>
         <Link to="/products">
           <button className="bg-black text-white px-5 py-3 rounded-xl text-sm hover:bg-gray-800 transition duration-300">
-            View All Products
+            View All Products &nbsp;→
           </button>
         </Link>
       </div>
