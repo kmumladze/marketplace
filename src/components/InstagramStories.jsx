6a9b512ea3ac344663a33b9497f7255db7a20587
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-03-04 20:26:46 +0400

    style: changed flex into grid

--- src/components/InstagramStories.jsx
+++ src/components/InstagramStories.jsx
@@ -9,11 +9,15 @@ export default function InstagramStories() {
       <h1 className="font-mono font-bold text-xl md:text-2xl dark:text-white">
         Our Instagram Stories
       </h1>
-      <div className="flex flex-col md:flex-row justify-center gap-10">
-        <img className="w-full md:w-60 h-auto" src={womanbag} alt="" />
-        <img className="w-full md:w-60 h-auto" src={fashionmodelwoman} alt="" />
-        <img className="w-full md:w-60 h-auto" src={fashionmodelman} alt="" />
-        <img className="w-full md:w-60 h-auto" src={makeup} alt="" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <img className="w-60 h-60 object-cover" src={womanbag} alt="" />
+        <img
+          className="w-60 h-60 object-cover"
+          src={fashionmodelwoman}
+          alt=""
+        />
+        <img className="w-60 h-60 object-cover" src={fashionmodelman} alt="" />
+        <img className="w-60 h-60 object-cover" src={makeup} alt="" />
       </div>
     </div>
   );
