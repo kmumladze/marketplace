e7eb020772d58054088ea3a31c2edc12b5f49a3c
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2025-03-07 16:05:21 +0400

    feat: pagination scroll top & cards skeleton

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 
 // import { Pagination } from "@heroui/pagination";
-import { Pagination } from "@heroui/react";
+import { Pagination, Card, Spacer } from "@heroui/react";
 
 import { Link, useSearchParams } from "react-router";
 import ProductCard from "./ProductCard";
@@ -23,10 +22,6 @@ import {
 import Header from "./Header.jsx";
 import FooterPage from "../pages/FooterPage.jsx";
 import UpperFooter from "./UpperFooter.jsx";
-import { Breadcrumbs, BreadcrumbItem } from "@heroui/react";
-import { Accordion, AccordionItem } from "@heroui/react";
-import { Slider } from "@heroui/react";
-import ScrollToTop from "./ScrollToTop.jsx";
 
 export default function Products() {
   const [products, setProducts] = useState(EMPTY_PRODUCTS);
@@ -68,26 +63,6 @@ export default function Products() {
     );
   }, [search, category]); //reset skip. always start with first page
 
-  //
-  // useEffect(() => {
-  //   async function fetchCategories() {
-  //     try {
-  //       const response = await fetch(
-  //         "https://dummyjson.com/products/category-list"
-  //       );
-  //       const resData = await response.json();
-  //       console.log(resData);
-
-  //       setProductsByCategory(resData);
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   }
-  //   fetchCategories();
-  // }, []);
-
-  //
-
   function addToCart(product) {
     setCart((prevCart) => [...prevCart, product]);
   }
@@ -117,68 +92,76 @@ export default function Products() {
     }
   }
 
+  if (!products) return <p>Loading...</p>;
+
   return (
     <section className="dark:bg-gray-900">
-      <ScrollToTop>
-        <Header />
+      <Header />
 
-        <main className="flex flex-col items-center mx-auto max-w-6xl w-full dark:bg-gray-900 dark:text-white">
-          <div className="flex flex-col gap-4 md:flex-row justify-center items-center py-6 gap-x-4 w-full">
-            <input
-              type="text"
-              placeholder="Search products..."
-              className="w-2/3 md:w-80 px-4 py-2 bg-gray-200 text-black focus:outline-none dark:bg-gray-600 dark:text-white"
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-            />
-            <Dropdown>
-              <DropdownTrigger className="dark:text-white">
-                <Button className="z-0 px-6 py-2 rounded-lg hover:bg-gray-200 transition">
-                  {selectedValue}
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                selectionMode="single"
-                selectedKeys={selectedKeys}
-                onSelectionChange={handleSortChange}
-                className="dark:text-white"
-              >
-                <DropdownItem key="Price(Low to High)">
-                  Sort by price: low to high
-                </DropdownItem>
-                <DropdownItem key="Price(High to Low)">
-                  Sort by price: high to low
-                </DropdownItem>
-                <DropdownItem key="Sort by ⇵">Reset</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </div>
+      <main className="flex flex-col items-center mx-auto max-w-6xl w-full dark:bg-gray-900 dark:text-white">
+        <div className="flex flex-col gap-4 md:flex-row justify-center items-center py-6 gap-x-4 w-full">
+          <input
+            type="text"
+            placeholder="Search products..."
+            className="w-2/3 md:w-80 px-4 py-2 bg-gray-200 text-black focus:outline-none dark:bg-gray-600 dark:text-white"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <Dropdown>
+            <DropdownTrigger className="dark:text-white">
+              <Button className="z-0 px-6 py-2 rounded-lg hover:bg-gray-200 transition">
+                {selectedValue}
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              selectionMode="single"
+              selectedKeys={selectedKeys}
+              onSelectionChange={handleSortChange}
+              className="dark:text-white"
+            >
+              <DropdownItem key="Price(Low to High)">
+                Sort by price: low to high
+              </DropdownItem>
+              <DropdownItem key="Price(High to Low)">
+                Sort by price: high to low
+              </DropdownItem>
+              <DropdownItem key="Sort by ⇵">Reset</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            {products.products.length === 0 ? (
-              <div className="col-span-2 md:col-span-4 text-center">
-                No products found
-              </div>
-            ) : (
-              products.products.map((product) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {products.products.length === 0
+            ? new Array(8).fill(0).map((el) => (
+                <Card className="w-[200px] space-y-5 p-4" radius="2xl">
+                  <div className="h-40 bg-default-300" />
+                  <div className="space-y-3">
+                    <div className="h-3 w-3/5 rounded-lg bg-default-200" />
+                    <div className="h-3 w-4/5 rounded-lg bg-default-200" />
+                    <div className="h-3 w-2/5 rounded-lg bg-default-300" />
+                  </div>
+                </Card>
+              ))
+            : products.products.map((product) => (
                 <div key={product.id} className="rounded-lg p-4">
                   <Link to={`/products/${product.id}`}>
                     <ProductCard product={product} />
                   </Link>
                 </div>
-              ))
-            )}
-          </div>
+              ))}
+        </div>
 
-          <Pagination
-            className="flex justify-center py-10"
-            showControls
-            page={currentPage}
-            total={Math.ceil((products.total || 1) / LIMIT)}
-            onChange={setCurrentPage}
-          />
-        </main>
-      </ScrollToTop>
+        <Pagination
+          className="flex justify-center py-10"
+          showControls
+          page={currentPage}
+          total={Math.ceil((products.total || 1) / LIMIT)}
+          onChange={(page) => {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+            setCurrentPage(page);
+          }}
+        />
+      </main>
       <UpperFooter />
       <FooterPage />
     </section>
